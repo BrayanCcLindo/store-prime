fa8c97b45a85f5c284b52ea7728b6e153c537334
BrayanCcari <xt9937@rimac.com.pe>
2025-10-23 16:02:11 -0500

    fix: cart actions

--- db/prisma.ts
+++ db/prisma.ts
@@ -20,25 +20,21 @@ export const prisma = new PrismaClient().$extends({
     },
     cart: {
       itemsPrice: {
-        needs: { itemsPrice: true },
         compute(cart: { itemsPrice: Prisma.Decimal }) {
           return cart.itemsPrice.toString();
         }
       },
       shippingPrice: {
-        needs: { shippingPrice: true },
         compute(cart: { shippingPrice: Prisma.Decimal }) {
           return cart.shippingPrice.toString();
         }
       },
       taxPrice: {
-        needs: { taxPrice: true },
         compute(cart: { taxPrice: Prisma.Decimal }) {
           return cart.taxPrice.toString();
         }
       },
       totalPrice: {
-        needs: { totalPrice: true },
         compute(cart: { totalPrice: Prisma.Decimal }) {
           return cart.totalPrice.toString();
         }
